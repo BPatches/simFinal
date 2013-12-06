1de74a19ae6e24592e0081457ed03463ae023c74
Patches Barringer <pbarring@mines.edu>
2013-12-06 15:10:39 -0700

    Finished log parser

--- logParser.rb
+++ logParser.rb
@@ -1,23 +1,41 @@
-$LOGSTEP = .1
+$LOGFREQ = 1 #THIS MUST BE A FLOAT
 class Pos
   attr_reader :x, :y
-  def initalize(x,y)
+  def initialize(x,y)
     @x = x
     @y = y
   end
 end
 
 class LogParser 
-  def initalize(fname)
-    @pedPos=Hash.new{ |hash,value| Hash[value]=List.new}
-    @carPos=Hash.new{ |hash,value| Hash[value]=List.new}
+  attr_reader :pedPos,:carPos,:lightState
+  def initialize(fname)
+    @pedPos=Hash.new{ |hash,value| hash[value]=Array.new}
+    @carPos=Hash.new{ |hash,value| hash[value]=Array.new}
     @lightState=Hash.new
     File.open(fname).each_line.with_index do |line,index|
-      carList = /{(\(.*\))*}.*/.match(line).to_a
-      puts carList
-      pedList = /{.*?}{(\(.*\))*}.*/.match(line).to_a
-      puts pedList
-      @lightState[(index/$LOGSTEP) ] = /{.*?}{.*?}{(.*?)}/.match(line).to_a[0]
+      
+      carList = /{((?:\(.*?\))*)}.*/.match(line)[1]
+      clistMatch = /\(([-\d]+),([-\d]+)\)(.*)/.match(carList)
+      while clistMatch do
+        @carPos[index/$LOGFREQ] << Pos.new(clistMatch[1].to_i,clistMatch[2].to_i)
+        clistMatch =  /\(([-\d]+),([-\d]+)\)(.*)/.match(clistMatch[3])
+      end
+      
+      pedList = /{.*?}{((?:\(.*?\))*)}.*/.match(line)[1]
+      plistMatch = /\(([-\d]+),([-\d]+)\)(.*)/.match(pedList)
+      while plistMatch do
+        @pedPos[index/$LOGFREQ] << Pos.new(plistMatch[1].to_i,plistMatch[2].to_i)
+        plistMatch =  /\(([-\d]+),([-\d]+)\)(.*)/.match(plistMatch[3])
+      end
+      
+      light = /{.*?}{.*?}{(.*?)}/.match(line)[1]
+      @lightState[index/$LOGFREQ] = light
     end
   end  
 end
+
+lp = LogParser.new("simLog.dat")
+puts lp.pedPos
+puts lp.carPos
+puts lp.lightState
