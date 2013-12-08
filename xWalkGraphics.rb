27a968f63720d3fbf28234e0bf1f491e03c9a910
Michael Mason <TeaUponTweed@localhost.localdomain>
2013-12-08 16:06:00 -0700

    lots o stuff

--- xWalkGraphics.rb
+++ xWalkGraphics.rb
@@ -9,21 +9,33 @@ class XWalkDisplay < Gosu::Window
 	def initialize(sim=nil)
 		@log = LogParser.new('simLog.dat')
 		@simWidth = 330*7
-		@simHeight = 100
+		@simHeight = 46+10
 		@winWidth = 1024
-		@winHeight = 128
+		@edge = 64
+		@winHeight = (@winWidth*@simHeight.to_f/@simWidth).round
 		@sim = sim
+		@fps = 30
 		@roadColor = Gosu::Color.argb(0x66666666)
-		super(@winWidth,@winHeight,false)
+		super(@winWidth,@winHeight+2*@edge,false,1000.0/@fps)
 		@i = 0
+		@timeWarp = 1
 		@pedImage = Gosu::Image.new(self,'man.bmp')
 		@carImage = Gosu::Image.new(self,'car.bmp')
+		@backgroundImage = Gosu::Image.new(self,'simFinal.bmp')
+		@lightPosX = @winWidth/2.0-64
+		@lightPosY = 0
 		@lightRed = Gosu::Image.new(self,'red.bmp')
 		@lightYellow = Gosu::Image.new(self,'yellow.bmp')
 		@lightGreen = Gosu::Image.new(self,'green.bmp')
+		@backgroundImage.draw(0,@edge,0)
 	end
 	def update
-		@i += 1
+		@i += @timeWarp
+		if button_down?(char_to_button_id('+'))
+			@timeWarp += 1
+		elsif button_down?(char_to_button_id('-')) && @timeWarp > 1
+			@timeWarp -= 1
+		end
 	end
 #	def needs_redraw?
 #		return @i < 3
@@ -32,9 +44,18 @@ class XWalkDisplay < Gosu::Window
 		#@tempCol = Gosu::Color.argb(0xff00ff00)
 		#draw_quad(0,0,@tempCol ,100,0,@tempCol ,0,100,@tempCol ,100,100,@tempCol )
 		drawBackground
+		drawBackgroundImage
+		#@backgroundImage.draw(0,@edge,0)
 		drawList(@log.pedPos[@i],@pedImage)
 		drawList(@log.carPos[@i],@carImage)
-
+		case @log.lightState
+		when "GREEN"
+			@lightGreen.draw(@lightPosX,@lightPosY,1)
+		when "YELLOW"
+			@lightYellow.draw(@lightPosX,@lightPosY,1)
+		when "RED"
+			@lightRed.draw(@lightPosX,@lightPosY,1)
+		end
 		#drawRoads
 		#drawBlocks
 		#drawXWalk
@@ -43,15 +64,21 @@ class XWalkDisplay < Gosu::Window
 		#for agent in log[i]
 		#end
 	end
+	def drawBackgroundImage
+		#@backgroundImage.draw(0,@edge,0)
+		
+		scale = @winWidth/@backgroundImage.width.to_f
+		@backgroundImage.draw(0,@edge,0,scale,scale)
+	end
+	def drawImage(image)
+
+	end
 	def drawBackground(color = Gosu::Color.argb(0xffffffff))
-		draw_quad(0,0,color,@winWidth,0,color,0,@winHeight,color,@winWidth,@winHeight,color,0,:default)
+		draw_quad(0,0,color,@winWidth,0,color,0,@winHeight+2*@edge,color,@winWidth,@winHeight+2*@edge,color,0,:default)
 	end
 	def drawRoads
 		drawRoad(@winHeight/2.0,true,50)
 		#drawRoad(@winWidth/2.0,false,50)
-		7.times{
-
-		}
 	end
 
 	def drawRoad(pos,ew,width,color = @roadColor)
@@ -71,8 +98,8 @@ class XWalkDisplay < Gosu::Window
 		for agent in list
 			x = agent.x
 			y = agent.y
-			drawX = x.to_f*20#/@simWidth*@winWidth
-			drawY = y.to_f*20#/@simHeight*@winHeight
+			drawX = x.to_f/@simWidth*@winWidth
+			drawY = y.to_f/@simHeight*@winHeight+@edge
 			image.draw(drawX,drawY,1)
 		end
 	end
